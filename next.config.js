28e997b2ed53f7e3b76b0cd992cb1d80c26077e5
Arwildo <arwildo@gmail.com>
2024-07-04 09:11:32 +0900

    build: setup build settings

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images : {
+    unoptimized: true,
+  },
+};
+ 
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <header className="bg-sky-100 py-20">
         <div className="container mx-auto text-center">
           <h1 className="text-4xl font-bold text-sky-900 mb-4">Bunda Pengharapan</h1>
-          <p className="text-xl text-sky-700">"Kasih yang Menyembuhkan"</p>
+          <p className="text-xl text-sky-700">&quot;Kasih yang Menyembuhkan&quot;</p>
         </div>
       </header>
 
