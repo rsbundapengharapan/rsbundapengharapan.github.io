91a37157c500c7acab57080cc407dc21ffbb1ff5
Arwildo <arwildo@gmail.com>
2025-02-09 12:38:17 +0900

    feat(StatusCard): update styles for improved layout and visual hierarchy

--- src/app/components/StatusCard.tsx
+++ src/app/components/StatusCard.tsx
@@ -23,15 +23,17 @@ const StatusCard: FC<StatusCardProps> = ({ type, title, subtitle, time }) => {
     };
 
     return (
-        <div className="bg-white rounded-3xl p-4 hover:shadow transition-shadow duration-300 my-2">
-            <div className="flex items-center mb-2">
+        <div className="bg-cuswhite rounded-3xl p-4 hover:shadow transition-shadow duration-300 my-2">
+            <div className="flex items-center justify-between mb-2">
+            <div className="flex items-center">
                 <div className="w-10 h-10 bg-cusblue rounded-full flex items-center justify-center">
-                    {renderIcon()}
+                {renderIcon()}
                 </div>
                 <h2 className="text-gray-900 text-md font-extrabold ml-2">{title}</h2>
             </div>
-            <p className="text-gray-500 text-sm">{subtitle}</p>
             <p className="text-gray-500 text-sm">{time}</p>
+            </div>
+            <p className="text-gray-500 text-sm">{subtitle}</p>
         </div>
     );
 };
