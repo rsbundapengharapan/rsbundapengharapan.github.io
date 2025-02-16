208c93ebf9e73dc095806a2f87eb4297940a0c5f
Arwildo <arwildo@gmail.com>
2025-02-16 22:40:02 +0900

    feat(Call, Footer): update Call component styling and enhance Footer with additional information

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,8 +6,9 @@ export default function Footer() {
     <footer className="py-8 bg-white rounded-b-3xl">
       <div className="container mx-auto text-center text-gray-600 flex flex-col items-center">
         <p className="text-gray-400 text-sm">&copy; RSBP 2001 - {currentYear}. All rights reserved.</p>
+        <p className="text-gray-400 text-sm my-2">Website build and maintain</p>
         <a href="https://arwildo.com" target="_blank" rel="noopener dofollow">
-          <Image src="/images/arwildo-software-cr.svg" alt="Arwildo Software" width={90} height={90} className="mt-4"/>
+          <Image src="/images/arwildo-software-cr.svg" alt="Arwildo Software" width={90} height={90} />
         </a>
       </div>
     </footer>
